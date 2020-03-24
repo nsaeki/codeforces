6a9064c239cf38ef2256db8d5ec21199bcf7085e
Nobuo Saeki <nobuo.saeki@gmail.com>
2020-03-25 01:41:14 +0900

    edu84-A after contest

--- edu84-d2/a.go
+++ edu84-d2/a.go
@@ -23,31 +23,9 @@ var sc = newScanner()
 
 func main() {
 	t := scanInt()
-	mx := 10000005
-	maxk := 0
-	sum := make([]int, mx/2)
-	sum[0], sum[1] = 0, 1
-	for i := 2; i < mx/2; i++ {
-		sum[i] = sum[i-1] + 2*i - 1
-		if sum[i] >= mx {
-			maxk = 2*i - 1
-			break
-		}
-	}
-
 	for i := 0; i < t; i++ {
 		n, k := scanInt(), scanInt()
-		res := false
-		if k < maxk {
-			if n&1 == 0 && k&1 == 0 && n >= sum[k] {
-				res = true
-			}
-			if n&1 == 1 && k&1 == 1 && n >= sum[k] {
-				res = true
-			}
-		}
-
-		if res {
+		if n&1 == k&1 && n/k >= k {
 			fmt.Println("YES")
 		} else {
 			fmt.Println("NO")
